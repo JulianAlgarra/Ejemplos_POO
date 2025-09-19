458d9cca62d74813e145b33c24bab3e8fd8ad6a2
Julian_Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2025-09-19 15:11:08 -0500

    Prueba

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP Para <b>ejecutar</b> el código, pulsar <shortcut actionId="Run"/> o
+// Haz clic en el ícono <icon src="AllIcons.Actions.Execute"/> del margen.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Pulsa <shortcut actionId="ShowIntentionActions"/> con tu cursor en el texto resaltado
+        // para ver cómo IntelliJ IDEA sugiere corregirlo.
+        System.out.println("Hello and welcome!");
+        System.out.println("sium");
+        System.out.println("Hello and welcome!");
+        System.out.println("sium");
+        System.out.println("Hello and welcome!");
+        System.out.println("sium");
+    }
+}
