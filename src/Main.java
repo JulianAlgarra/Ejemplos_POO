13fa808989d84ff835f7e3c29fefb3076b717203
Julian_Algarra <jalejandroalgarra@ucundinamarca.edu.co>
2025-09-25 17:51:41 -0500

    Ejemplo listas

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,78 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        menu();
+        menu_cuentas();
 
 
     }
 
+    public static void menu_cuentas(){
+        Scanner opcion = new Scanner(System.in);
+        Scanner datos= new Scanner(System.in);
+
+        int opcion_cuenta=0;
+
+        List<Cuenta>lstLista=new ArrayList<>();
+        while (opcion_cuenta!=5) {
+            Cuenta p1= new Cuenta();
+            System.out.println("Que accion desea realizar");
+            System.out.println("1. Crear Cuenta");
+            System.out.println("2. Mostrar Cuentas");
+            System.out.println("3. Eliminar cuentas");
+            System.out.println("4. Buscar cuenta");
+            System.out.println("5. Salir ");
+            opcion_cuenta= opcion.nextInt();
+            switch (opcion_cuenta){
+                case 1 :
+                    System.out.println("Ingrese su numero de cuenta");
+                    p1.setNumero_cuenta(datos.next());
+                    System.out.println("Ingrese su numero de cedula");
+                    p1.setCedula_usuario(datos.next());
+                    System.out.println("Ingrese su saldo actual");
+                    p1.setSaldo(datos.nextDouble());
+                    lstLista.add(p1);
+                    break;
+                case 2:
+                    for (Cuenta c :lstLista){
+                        System.out.println(c);
+                    }
+                    break;
+                case 3:
+                    Scanner eliminar_cuenta =new Scanner(System.in);
+                    String eliminar;
+                    System.out.println("Ingrese el numero de cuenta que desee eliminar");
+                    eliminar= eliminar_cuenta.next();
+                    for (Cuenta c :lstLista){
+                        if (lstLista.isEmpty()){
+                            System.out.println("No hay ninguna cuenta");
+                        }else if (c.getNumero_cuenta().equalsIgnoreCase(eliminar)){
+                            lstLista.remove(c);
+                            System.out.println("La cuenta ha sido eliminada");
+                        }
+                    }
+                    break;
+                case 4:
+                    Scanner buscar_cuenta =new Scanner(System.in);
+                    String buscar;
+                    System.out.println("Ingrese el numero de cuenta que desee buscar");
+                    buscar = buscar_cuenta.next();
+                    for (Cuenta c :lstLista){
+                            if (lstLista.isEmpty()){
+                                System.out.println("No hay ninguna cuenta");
+                            }else if (c.getNumero_cuenta().equalsIgnoreCase(buscar)){
+                                menu();
+                            }
+                    }
+                    break;
+            }
+        }
+
+    }
     public static void menu(){
+        Cuenta p1= new Cuenta();
         Scanner opcion= new Scanner(System.in);
         int op = 0;
-
-
-        Scanner datos= new Scanner(System.in);
-        Cuenta p1= new Cuenta();
-
-        System.out.println("Ingrese su numero de cuenta");
-        p1.setNumero_cuenta(datos.next());
-        System.out.println("Ingrese su numero de cedula");
-        p1.setCedula_usuario(datos.next());
-        System.out.println("Ingrese su saldo actual");
-        p1.setSaldo(datos.nextDouble());
-
-
         while (op !=5){
 
             System.out.println("Bienvenido a su cuenta");
